9b36c233024f610c7db4d270d64a0b256dbf7804
Jeff Simons Decena <jeff.decena@yahoo.com>
2023-08-25 02:02:05 +1200

    Fix: Add the sky icon

--- frontend/src/components/ResultList.tsx
+++ frontend/src/components/ResultList.tsx
@@ -13,9 +13,10 @@ const ResultList = ({ items, setItem, setHasSelected } : ResultListProp) => {
     <table className="w-full text-sm text-left text-gray-500 mt-10">
       <thead className="text-xs text-gray-700 uppercase bg-gray-50">
         <tr>
+          <th>Sky</th>
           <th scope="col" className="px-6 py-3">
             Day
-          </th>
+          </th>          
           <th scope="col" className="px-6 py-3">
             Temp
           </th>
@@ -30,11 +31,12 @@ const ResultList = ({ items, setItem, setHasSelected } : ResultListProp) => {
             setItem(item)
             setHasSelected(true)
           }}>
+            <th><img src={`http://openweathermap.org/img/w/${item.weather[0].icon}.png`} width={24} height={24} /></th>
             <th
               scope="row"
               className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
             >
-              {DateTime.fromSeconds(item.dt).toFormat('ccc, dd y')}
+              {DateTime.fromSeconds(item.dt).toLocaleString(DateTime.DATE_HUGE)}
             </th>
             <td className="px-6 py-4">{item.main.temp} °C</td>
             <td className="px-6 py-4">{item.weather[0].description}</td>
